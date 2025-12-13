586e7889bcc22e3f3af143b7ded7dbed93af7daa
Inderjeet Singh Talwandi Sabo <ins.ddrive@gmail.com>
2025-12-13 17:03:55 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,41 @@
-﻿const CACHE_NAME = 'karah-parshad-v2';
+const CACHE_NAME = 'karah-parshad-v3';
 const ASSETS_TO_CACHE = [
- './',
- './index.html',
- './manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-// Install Event
 self.addEventListener('install', (event) => {
- event.waitUntil(
-   caches.open(CACHE_NAME)
-     .then((cache) => {
-       return cache.addAll(ASSETS_TO_CACHE);
-     })
- );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
 });
 
-// Fetch Event
 self.addEventListener('fetch', (event) => {
- event.respondWith(
-   caches.match(event.request)
-     .then((response) => {
-       return response || fetch(event.request);
-     })
- );
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
 
-// Activate Event
 self.addEventListener('activate', (event) => {
- event.waitUntil(
-   caches.keys().then((cacheNames) => {
-     return Promise.all(
-       cacheNames.map((cache) => {
-         if (cache !== CACHE_NAME) {
-           return caches.delete(cache);
-         }
-       })
-     );
-   })
- );
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
+
+
